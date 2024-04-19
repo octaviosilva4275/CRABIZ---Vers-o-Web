04b08be05aec2331e1a2103a232b085a61297b90
Octavio Augusto <octavio.freitas4275@gmail.com>
2024-04-19 09:41:56 -0300

    Subir na web

--- conexao.py
+++ conexao.py
@@ -3,7 +3,7 @@ import mysql.connector
 class Conexao:
     def conectar():
         mydb = mysql.connector.connect(
-            host="10.110.140.166",
+            host="projetochat2.mysql.database.azure.com",
             user="equipe",
             password="123456789",
             database="CRABIZ"
