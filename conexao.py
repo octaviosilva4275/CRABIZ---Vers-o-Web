982e1e4a82d6e186347957cb114194e41719914a
Octavio Augusto <octavio.freitas4275@gmail.com>
2024-04-19 09:37:07 -0300

    Conexao

--- conexao.py
+++ conexao.py
@@ -3,7 +3,7 @@ import mysql.connector
 class Conexao:
     def conectar():
         mydb = mysql.connector.connect(
-            host="10.110.140.136",
+            host="10.110.140.166",
             user="equipe",
             password="123456789",
             database="CRABIZ"
